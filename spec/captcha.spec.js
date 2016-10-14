da82743754d101712ac0ba16f54e0bc208a62b4c
NarezTwentyFour <Narez.Jaroenphum@gmail.com>
2016-10-14 18:28:33 +0700

    Create program (class Captcha,Operator,Leftoperand,Rightoperand) and test case 1

---
+++ spec/captcha.spec.js
@@ -0,0 +1,38 @@
+function Captcha(pattern , opearator , leftoperand ,rightoperand){
+
+  this.generate = () =>{
+    if(pattern === 1){
+        return leftoperand + ' ' + Operator(opearator) + ' ' + Rightoperand(rightoperand);
+      }
+    else{
+        return Leftoperand(leftoperand) + ' ' + Operator(opearator) + ' ' + rightoperand;
+    }
+  }
+}
+function Operator(opearator){
+  let opearatortostring = ['+','-']
+  return opearatortostring[opearator-1];
+}
+function Leftoperand(leftoperand){
+  let valuetostring = ['ZERO','ONE','TWO','THREE','FOUR','FIVE','SIX','SEVEN','EIGHT','NINE'];
+  return valuetostring[leftoperand];
+}
+function Rightoperand(rightoperand){
+  let valuetostring = ['ZERO','ONE','TWO','THREE','FOUR','FIVE','SIX','SEVEN','EIGHT','NINE'];
+  return valuetostring[rightoperand];
+}
+
+describe('Captcha App',() => {
+	describe('Pattern 1',() => {
+    const pattern = 1  /*เป็นการกำหนดค่าคงที่ห้ามเปลี่ยนแปลงค่า*/
+    it('should return "2 + FIVE" when input is 1,1,2,5',() =>{
+      expect(createApp(pattern,1,2,5).generate()).toEqual('2 + FIVE')
+    })
+	})
+  /*describe('Pattern 2',() => {
+    const pattern = 2
+  })*/
+})
+function createApp(pattern ,opearator , leftOperand ,RightOperand){
+	return new Captcha(pattern , opearator , leftOperand ,RightOperand)
+}
